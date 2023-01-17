9912d9773f163a005ebf7232ada810cd34a82994
anurmamedov <sdeveloperanar@gmail.com>
2023-01-16 20:15:14 -0500

    design of footer template

--- posts/views.py
+++ posts/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def posts(request):
-    return render(request,"posts/footer.html")
+    return render(request,"footer.html")
 
 def post_detail(request):
     return HttpResponse("Hello post detail page!")
