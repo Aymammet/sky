95dcb2d70b7156dc4968501cce8dd6798e1ca035
anurmamedov <sdeveloperanar@gmail.com>
2023-02-08 21:49:55 -0500

    post model commit

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class Post(AbstractUser):
+    title = models.CharField(max_length=50, blank=False, null=True)
+    owner = models.CharField(max_length=500, blank=False, null=True)
+
+    def __str__(self):
+        return self.title
