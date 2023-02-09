8df0f9e98a3028b1bee6ea5f918545feef8d3e13
anurmamedov <sdeveloperanar@gmail.com>
2023-02-08 22:49:06 -0500

    posts models created

--- posts/models.py
+++ posts/models.py
@@ -1,10 +1,14 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from profiles.models import User
 
 # Create your models here.
-class Post(AbstractUser):
-    title = models.CharField(max_length=50, blank=False, null=True)
-    owner = models.CharField(max_length=500, blank=False, null=True)
+class Post(models.Model):
+    title = models.CharField(max_length=50, blank=False)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE,)
+    image = models.ImageField(upload_to=None, height_field=None, width_field=None, max_length=100, blank=True, null=True)
+    created_date = models.DateField(auto_now_add=True)
+    total_likes = models.PositiveIntegerField(default=0)
+
 
     def __str__(self):
         return self.title
