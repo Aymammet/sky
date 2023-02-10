c22cae921951f3c206897c278f36f645c657b550
anurmamedov <sdeveloperanar@gmail.com>
2023-02-10 00:05:14 -0500

    create Customview

--- main/views.py
+++ main/views.py
@@ -1,9 +1,21 @@
 # Create your views here.
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import CreateView
+from profiles.models import User
+from django.urls import reverse_lazy
+from .forms import UserForm
 
 def login(request):
      return render(request, 'login.html')
 
-def register(request):
-     return render(request, 'register.html')
+class CustomRegisterView(CreateView):
+     model = User
+     form_class = UserForm
+     template_name = 'register.html'
+     success_url = reverse_lazy('main:login')
+
+     def post(self, request, *args, **kwargs):
+          data= request.POST
+          # print(data)
+          return
