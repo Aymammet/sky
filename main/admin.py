0e954ef49a8e6d002fec6097e7d6159abbf444f1
anurmamedov <sdeveloperanar@gmail.com>
2023-02-09 22:51:47 -0500

    admin moved from main into profiles

--- main/admin.py
+++ main/admin.py
@@ -1,8 +1,1 @@
 from django.contrib import admin
-from profiles.models import User
-
-# Register your models here.
-class Admin(admin.ModelAdmin):
-    list_display = ('first_name', 'last_name', 'username', 'email')
-
-admin.site.register(User, Admin)
--- profiles/admin.py
+++ profiles/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import User
 
+# Register your models here.
+class Admin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'username', 'email')
+
+admin.site.register(User, Admin)
 # Register your models here.
