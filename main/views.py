7bdada6b2c95b8878daff626a18e86e8391c76b2
anurmamedov <sdeveloperanar@gmail.com>
2023-02-14 14:17:10 -0500

    Create register view and form updated in register.html

--- main/views.py
+++ main/views.py
@@ -1,10 +1,11 @@
 # Create your views here.
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.views.generic import CreateView
 from profiles.models import User
 from django.urls import reverse_lazy
 from .forms import UserForm
+from posts.views import posts
 
 def login(request):
      return render(request, 'login.html')
@@ -15,7 +16,19 @@ class CustomRegisterView(CreateView):
      template_name = 'register.html'
      success_url = reverse_lazy('main:login')
 
+     def get(self, request):
+          if request.user.is_authenticated:
+               return redirect('posts')
+          else:
+               return render(request, self.template_name)
+
+
      def post(self, request, *args, **kwargs):
-          data= request.POST
-          # print(data)
-          return
+          # data= request.POST
+          name = request.POST.get('username')
+          if request.user.is_authenticated:
+               return redirect('posts')
+          else:
+               return redirect('posts')
+               # form = self.form_class(request.POST)
+               return
