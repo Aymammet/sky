706f0b66641a9c725413257a76c0dedbfffb6367
anurmamedov <sdeveloperanar@gmail.com>
2023-02-17 11:24:12 -0500

    Register page updated

--- main/views.py
+++ main/views.py
@@ -2,13 +2,16 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.views.generic import CreateView
+from django.core.exceptions import ValidationError
 from profiles.models import User
 from django.urls import reverse_lazy
 from .forms import UserForm
 from posts.views import posts
 
+
 def login(request):
      return render(request, 'login.html')
+
 
 class CustomRegisterView(CreateView):
      model = User
@@ -22,13 +25,16 @@ class CustomRegisterView(CreateView):
           else:
                return render(request, self.template_name)
 
-
      def post(self, request, *args, **kwargs):
-          # data= request.POST
-          name = request.POST.get('username')
+          form = self.form_class(request.POST, request.FILES)
+          password = request.POST.get('password')
+          password2 = request.POST.get('password2')
           if request.user.is_authenticated:
                return redirect('posts')
           else:
-               return redirect('posts')
-               # form = self.form_class(request.POST)
-               return
+               if password != password2:
+                    form.add_error("password", "Your password inputs doesn't match, please correct it!")
+               # if len(username) < 5 or len(username) > 20:
+               #      form.add_error("username", "Your username should be longer than 5 and less than 20 letters & symbols")
+               return render(request, self.template_name, {'form': form})
+               
--- profiles/models.py
+++ profiles/models.py
@@ -1,8 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.core.exceptions import ValidationError
 
-# Create your models here.
+def validate_username_length(value):
+    if len(value) < 3:
+        raise ValidationError('Username must be at least 3 characters')
+
+
 class User(AbstractUser):
+    username = models.CharField(max_length=15, unique=True, validators=[validate_username_length])
     email = models.EmailField(unique=True, blank=False)
     profession = models.CharField(max_length=50, blank=True, null=True)
     bio = models.TextField(max_length=500, blank=True, null=True)
