34b0444c7f64066d1b4d197642e6d74712b942ef
anurmamedov <sdeveloperanar@gmail.com>
2023-02-20 15:31:43 -0500

    error included for existing username

--- main/views.py
+++ main/views.py
@@ -27,6 +27,7 @@ class CustomRegisterView(CreateView):
 
      def post(self, request, *args, **kwargs):
           form = self.form_class(request.POST, request.FILES)
+          username = request.POST.get('username')
           password = request.POST.get('password')
           password2 = request.POST.get('password2')
           if request.user.is_authenticated:
@@ -38,3 +39,5 @@ class CustomRegisterView(CreateView):
                if form.is_valid():
                     form.save()
                     return redirect('posts')
+               if User.objects.filter(username=username).exists:
+                    return render(request, self.template_name, {'form': form})
