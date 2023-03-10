26c733b34ef69ea2a3144a985245c1fadd83075f
anurmamedov <sdeveloperanar@gmail.com>
2023-03-09 22:46:56 -0500

    post-edit template and view

--- posts/urls.py
+++ posts/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 from . import views
-from .views import PostListView
+from .views import PostListView, PostEditView
 
 urlpatterns = [
     path('', PostListView.as_view(), name='posts'),
     path('detail/', views.post_detail, name='post_detail'),
-    path('edit/', views.post_edit, name='post_edit'),
+    path('edit/', PostEditView.as_view(), name='post_edit'),
     path('create/', views.post_create, name='post_create'),
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from profiles.models import User
 from posts.models import Post
-from django.views.generic import ListView
+from django.views.generic import ListView, CreateView, UpdateView
+from django.urls import reverse_lazy
+from .forms import PostForm
 
 
 # Create your views here.
@@ -14,6 +16,27 @@ class PostListView(ListView):
           base_query = super().get_queryset()
           data = base_query.order_by('created_date')
           return data
+     
+class PostEditView(CreateView, UpdateView):
+    model = Post
+    template_name = 'post-edit.html'
+    context_object_name = 'post'
+    fields = ['title', 'image']
+    success_url = reverse_lazy('posts')
+    form = PostForm
+
+    def get(self, request):
+        if request.user.is_authenticated:
+            return render(request, self.template_name)
+        else:
+            return render(request, 'login.html')
+        
+    def form_valid(self, form):
+        myobj = form.save(commit=False)
+        myobj.owner = self.request.user
+        return super().form_valid(form)
+    
+
 
 def post_detail(request):
     return HttpResponse("Hello post detail page!")
