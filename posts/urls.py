c5459972e2575e686450fb43039c6e9730f054a6
anurmamedov <sdeveloperanar@gmail.com>
2023-03-10 23:24:38 -0500

    post edit template and view updated

--- posts/urls.py
+++ posts/urls.py
@@ -5,6 +5,6 @@ from .views import PostListView, PostCreateview, PostEditView
 urlpatterns = [
     path('', PostListView.as_view(), name='posts'),
     path('detail/', views.post_detail, name='post_detail'),
-    path('<int>:pk>/edit/', PostEditView.as_view(), name='post_edit'),
+    path('<int:pk>/edit/', PostEditView.as_view(), name='post_edit'),
     path('create/', PostCreateview.as_view(), name='post_create'),
 ]
--- posts/views.py
+++ posts/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from profiles.models import User
 from .models import Post
-from django.views.generic import ListView, CreateView
+from django.views.generic import ListView, CreateView, UpdateView
 from django.urls import reverse_lazy
 from .forms import PostForm
 
@@ -18,7 +18,7 @@ class PostListView(ListView):
           data = base_query.order_by('-created_date', '-created_time')
           return data
      
-class PostEditView(CreateView):
+class PostEditView(UpdateView):
     model = Post
     template_name = 'post-edit.html'
     context_object_name = 'post'
@@ -26,19 +26,20 @@ class PostEditView(CreateView):
     success_url = reverse_lazy('posts')
     form = PostForm
 
-    def get_object(self, request):
+    def get_object(self, queryset=None):
         id = self.kwargs.get('pk')
-        return Post.posts.get(pk=id)
+        return Post.objects.get(id=id)
     
           
     def form_valid(self, form):
-        myobj = form.save(commit=False)
-        myobj.owner = self.request.user
+        mypost = form.save(commit=False)
+        mypost.owner = self.request.user
         response = super().form_valid(form)
 
         if form.cleaned_data['image']:
-            self.post.image.name = form.cleaned_data['data'].name
-            self.post.save()
+            mypost.title = form.cleaned_data['title']
+            mypost.save()
+        return response
 
     
 
