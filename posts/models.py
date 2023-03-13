b31d6c666d691e1036a67f744abe7f182b7e928a
anurmamedov <sdeveloperanar@gmail.com>
2023-03-13 00:24:04 -0400

    validators added in post edit and create, errors added, button changed and design update

--- posts/models.py
+++ posts/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 from profiles.models import User
+from django.core.validators import MinLengthValidator
 
 # Create your models here.
 class Post(models.Model):
-    title = models.CharField(max_length=500, blank=False)
+    title = models.CharField(max_length=500, blank=False, validators=[MinLengthValidator(3)])
     owner = models.ForeignKey(User, on_delete=models.CASCADE,)
     image = models.ImageField(upload_to='post_images', max_length=100, blank=True, null=True)
     created_date = models.DateField(auto_now_add=True)
