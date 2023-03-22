e8676af8ba577b2dcff4fd39c21babebb4ab05c8
Aymammet Annamyradov <aymammet2018@gmail.com>
2023-03-21 23:31:23 -0400

    profile image field is fixed

--- profiles/models.py
+++ profiles/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.core.exceptions import ValidationError
 from django.core.validators import MinLengthValidator
+from django.conf import settings
 
 class User(AbstractUser):
     username = models.CharField(max_length=15, unique=True, validators=[MinLengthValidator(3)])
@@ -10,6 +11,7 @@ class User(AbstractUser):
     bio = models.TextField(max_length=500, blank=True, null=True)
     birth_date = models.DateField(null=True, blank=True)
     gender = models.CharField(max_length=6, choices=[('Male', 'Male'),('Female', 'Female'),('Other', 'Other')], blank=True)
+    profile_image = models.ImageField(upload_to='profile_images/', default="profile_images/default_profile_image.png")
 
     def __str__(self):
         return self.username
