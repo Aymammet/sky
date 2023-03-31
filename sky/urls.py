bcbceacde67d23310a92b99a7a68c6e8a5591724
anurmamedov <sdeveloperanar@gmail.com>
2023-03-31 11:48:36 -0400

    Redirect view added for main page

--- sky/urls.py
+++ sky/urls.py
@@ -15,15 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-
+from django.views.generic.base import RedirectView
 from sky import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path("", include("main.urls")),
-    path("", include("posts.urls")),    
+    path('', include('main.urls')),
+    path('', RedirectView.as_view(url='/posts')),    
     path('admin/', admin.site.urls),
-    path("profile/", include("profiles.urls")),
-    path("posts/", include("posts.urls")),
+    path('profile/', include('profiles.urls')),
+    path('posts/', include('posts.urls')),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
